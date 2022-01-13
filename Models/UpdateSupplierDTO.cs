bf3ea8f0ae26d126951752a0e09f134f56cf6142
Mithun <Mithunsharmawins@gmail.com>
2022-01-13 13:07:55 +0530

    Api Model Uploaded

--- Models/UpdateSupplierDTO.cs
+++ Models/UpdateSupplierDTO.cs
@@ -1,16 +1,19 @@
 namespace EE.Models
 {
     public class UpdateSupplierDTO
-    {  
-        public int Supplier_Id {get; set;}            
-        public string First_Name {get; set;}        
-        public string Last_Name {get; set;}                    
-        public string Phone_Number {get; set;}                    
-        public string City {get; set;}
-        public string Company_Name {get; set;}          
-        public string Company_Address {get; set;}  
-        public int Category_Id {get; set;}  
-        public bool IsActive {get; set;}
-        public string ActionPerformedBy {get; set;}       
+    {
+        public int Supplier_Id{get;set;}
+          public string First_Name{get;set;}
+        public string Last_Name{get;set;}
+        public string Phone_Number{get;set;}
+        // public string Street_Address{get;set;}
+        // public string State{get;set;}
+        // public string City{get;set;}
+        // public string Zip_Code{get;set;}
+        public string Company_Name{get;set;}
+        public string Company_Address{get;set;}
+        public int Category_Id{get;set;}
+        public bool IsActive {get;set;}
+         public string ActionPerformedBy {get;set;}
     }
 }
